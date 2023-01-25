640ed072192a422b311fb333455e44eef5c8f98d
irfanjamil <irfanjamil9@gmail.com>
2023-01-25 16:10:44 -0500

    adding configurations to be able to connect to mysql db

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import pymysql
 
 free_api_key = 'c7a9fea03571e4119838f7d7091cf679'
 base_url = 'https://api.the-odds-api.com/v4/sports/'
+dbinstance_endpoint = 'database.cl4b0hojqcsj.us-east-1.rds.amazonaws.com'
+db_username = 'admin'
+db_pw = 'pinnacle'
+db_name = 'bets'
 
 class APIinterface():
 
-    def __init__(self, hostendpoint, apikey):
+    def __init__(self, hostendpoint, apikey, serverhost, uname, pw, db):
         self.endpoint = hostendpoint
         self.apikey = apikey
         self.sport_key_dict = {}
+        self.connection = pymysql.connect(host = serverhost, user = uname, password = pw, database = db, autocommit=True)
+        self.cursor = self.connection.cursor()
     
     def get_inseason_sports(self):
         if len(self.sport_key_dict) != 0:
@@ -22,6 +28,9 @@ class APIinterface():
             self.sport_key_dict[cur_sport]=cur_key
         
         print(list(self.sport_key_dict.values()))
+    
+    def create_ML_table(self):
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS moneyline (id VARCHAR(100), Home_Team VARCHAR(100), Home_Team_Dec_Odds FLOAT(7,2), Away_Team VARCHAR(100), Away_Team_Dec_Odds FLOAT(7,2), Start_Time TIMESTAMP')
     
     def getMLodds(self, sport_key):
         if sport_key not in list(self.sport_key_dict.values()):
