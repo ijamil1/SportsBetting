07aa8eac6b7f30e22013717298c27db42bb82716
irfanjamil <irfanjamil9@gmail.com>
2023-01-28 15:24:11 -0500

    added functions to initialize balance for sportsbook accounts, to get them and display them, and to also update the balances

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class APIinterface():
         self.sport_key_dict = {}
         self.connection = pymysql.connect(host = credentials[1], user = credentials[2], password = credentials[3], database = credentials[4], autocommit=True)
         self.cursor = self.connection.cursor()
+        self.mybooks = ['BetUS','DraftKings','Barstool Sportsbook','BetMGM','BetRivers','FanDuel','FOX Bet','Unibet','William Hill (US)']
     
     def get_inseason_sports(self):
         if len(self.sport_key_dict) != 0:
@@ -30,7 +31,7 @@ class APIinterface():
         print(list(self.sport_key_dict.values()))
     
     def create_ML_table(self):
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS moneyline (id VARCHAR(100), sport_key VARCHAR(50), Home_Team VARCHAR(100), Home_Team_Dec_Odds FLOAT(7,2), Away_Team VARCHAR(100), Away_Team_Dec_Odds FLOAT(7,2), book  VARCHAR(50), Start_Time DATETIME, Insert_Time DATETIME, CONSTRAINT id_book PRIMARY KEY (id,book))')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS moneyline (id VARCHAR(100), sport_key VARCHAR(50), Home_Team VARCHAR(100), Home_Team_Dec_Odds FLOAT(7,2), Away_Team VARCHAR(100), Away_Team_Dec_Odds FLOAT(7,2), book  VARCHAR(50), Start_Time DATETIME, Insert_Time DATETIME, CONSTRAINT id_book_time PRIMARY KEY (id,book, Insert_Time))')
 
     def create_balance_table(self):
         self.cursor.execute('CREATE TABLE IF NOT EXISTS balance (book VARCHAR(50), amount FLOAT(7,2))')
@@ -53,6 +54,8 @@ class APIinterface():
 
             for book in bookies:
                 book_name = book['title']
+                if book_name not in self.mybooks:
+                    continue
                 outcomes = book['markets'][0]['outcomes']
                 
                 for outcome in outcomes:
@@ -81,6 +84,26 @@ class APIinterface():
         for row in rows:
             print(row[2],row[3],row[4],row[5],row[6],row[7], sep=', ')
         
+
+    def getBalance(self):
+        self.cursor.execute('SELECT * FROM balance')
+        rows = self.cursor.fetchall()
+
+        for row in rows:
+            print(row[0],'$'+str(row[1]),sep=',')
+    
+    def updateBalance(self, book, amount):
+        self.cursor('select amount from balance where book = \'{}\''.format(book))
+        row = self.cursor.fetchall()
+        cur_amt = float(row[0][0])
+        new_amt = cur_amt + amount
+        self.cursor('update balance set amount = {} where book = \'{}\''.format(new_amt,book))
+    
+    def  initalizeBalance(self):
+        for book in self.mybooks:
+            self.cursor.execute('INSERT INTO balance VALUES (\'{}\',{})'.format(book,100.00))
+    
+
 
                  
         
