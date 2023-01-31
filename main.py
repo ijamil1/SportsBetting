012b15351fb2f14d976242484eacc22284cc1c8f
irfanjamil <irfanjamil9@gmail.com>
2023-01-31 16:29:15 -0500

    added rough documentation txt file and added functionality that uploads scores for games that i bet on and adjust book balances accordingly; also added functionality that checks balance is available before making a bet; last function i added was a function that deletes from the odds tables games that have happened

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pymysql
 import datetime 
 
 
-class APIinterface():
+class API():
 
     def __init__(self, hostendpoint='https://api.the-odds-api.com/v4/sports/'):
         with open('credentials.txt') as f:
@@ -31,10 +31,17 @@ class APIinterface():
         print(list(self.sport_key_dict.values()))
     
     def create_scores_table(self):
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS scores (id VARCHAR(100), Home_Team VARCHAR(100), Home_Team_Score FLOAT(7,2), Away_Team VARCHAR(100), Away_Team_Score  FLOAT(7,2))')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS scores (id VARCHAR(100), Home_Team VARCHAR(100), Home_Team_Score FLOAT(7,2), Away_Team VARCHAR(100), Away_Team_Score  FLOAT(7,2) CONSTRAINT id_pk PRIMARY KEY (id))')
 
     def create_ML_table(self):
         self.cursor.execute('CREATE TABLE IF NOT EXISTS moneyline (id VARCHAR(100), sport_key VARCHAR(50), Home_Team VARCHAR(100), Home_Team_Dec_Odds FLOAT(7,2), Away_Team VARCHAR(100), Away_Team_Dec_Odds FLOAT(7,2), book  VARCHAR(50), Start_Time DATETIME, Insert_Time DATETIME, CONSTRAINT id_book_time PRIMARY KEY (id,book, Insert_Time))')
+
+    def delete_from_tables(self):
+        score_ids = self.getIdsScoresTbl()
+        for id in score_ids:
+            self.cursor.execute('delete from moneyline where id = \'{}\''.format(id))
+            self.cursor.execute('delete from spreads where id = \'{}\''.format(id))
+        
     
     def create_spreads_table(self):
         self.cursor.execute('CREATE TABLE IF NOT EXISTS spreads (id VARCHAR(100), sport_key VARCHAR(50), Home_Team VARCHAR(100), Home_Team_Spread FLOAT(7,2), Home_Team_Odds FLOAT(7,2), Away_Team VARCHAR(100), Away_Team_Spread FLOAT(7,2), Away_Team_Odds FLOAT(7,2), book  VARCHAR(50), Start_Time DATETIME, Insert_Time DATETIME, CONSTRAINT id_book_time PRIMARY KEY (id,book, Insert_Time))')
@@ -135,14 +142,24 @@ class APIinterface():
             print(row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9], sep=', ')
 
 
-    def getBalance(self):
+    def getBalance(self, book=None):
+        if book:
+            if book not in self.mybooks:
+                print('error -- invalid book passed')
+                return 
+            self.cursor.execute('SELECT * FROM balance where book = \'{}\''.format(book))
+            return float(self.cursor.fetchall()[0][0])
         self.cursor.execute('SELECT * FROM balance')
         rows = self.cursor.fetchall()
 
         for row in rows:
             print(row[0],'$'+str(row[1]),sep=',')
-    
+        return
+
     def updateBalance(self, book, amount):
+        if book not in self.mybooks:
+            print('error -- invalid book passed')
+            return 
         self.cursor('select amount from balance where book = \'{}\''.format(book))
         row = self.cursor.fetchall()
         cur_amt = float(row[0][0])
@@ -155,35 +172,53 @@ class APIinterface():
     
 
     def createSpreadBetsTable(self):
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS spread_bets (id VARCHAR(100),team_bet_on VARCHAR(100), book_bet_at VARCHAR(100), amount_bet FLOAT(7,2), spread_offered FLOAT(7,2), line_offered FLOAT(7,2))')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS spread_bets (id VARCHAR(100),team_bet_on VARCHAR(100), book_bet_at VARCHAR(100), amount_bet FLOAT(7,2), spread_offered FLOAT(7,2), line_offered FLOAT(7,2) CONSTRAINT id_book_team PRIMARY KEY (id,team_bet_on,book_bet_at))')
     
     def uploadSpreadBets(self, id, team, book,amount,spread,dec_odds):
+        if amount > self.getBalance(book):
+            print('not enough money at book to bet this amount')
+            return
+        self.updateBalance(book, -1*amount)
         self.cursor.execute('INSERT INTO spread_bets VALUES (\'{}\',\'{}\',\'{}\',{},{},{})'.format(id,team,book,amount,spread,dec_odds))
-
+        
     def createMLBetsTable(self):
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS ml_bets (id VARCHAR(100),team_bet_on VARCHAR(100), book_bet_at VARCHAR(100), amount_bet FLOAT(7,2), line_offered FLOAT(7,2))')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS ml_bets (id VARCHAR(100),team_bet_on VARCHAR(100), book_bet_at VARCHAR(100), amount_bet FLOAT(7,2), line_offered FLOAT(7,2) CONSTRAINT id_book_team PRIMARY KEY (id,team_bet_on,book_bet_at))')
 
     def uploadMLBets(self, id, team, book, amt, dec_odds):
+        if amt > self.getBalance(book):
+            print('not enough money at book to bet this amount')
+            return
+        self.updateBalance(book, -1*amt)
         self.cursor.execute('INSERT INTO ml_bets VALUES (\'{}\',\'{}\',\'{}\',{},{})'.format(id, team, book, amt, dec_odds))
     
-    def getScores(self, sportkey):
+    def getIdsScoresTbl(self):
+        self.cursor.execute('select id from scores')
+        rows = self.cursor.fetchall()
+        ls = []
+        for row in rows:
+            ls.append(row[0])
+        return ls
+    
+    def uploadScores(self, sportkey):
         json = requests.get(self.endpoint+sportkey+'/scores/',params={'apiKey':self.apikey,'daysFrom':3}).json()
-        bet_ids = []
+        spreadbet_ids = []
+        mlbet_ids = []
+        score_ids = self.getIdsScoresTbl()
         self.cursor.execute('select id from spread_bets')
         rows = self.cursor.fetchall()
         for row in rows:
-            bet_ids.append(row[0])
+            spreadbet_ids.append(row[0])
         self.cursor.execute('select id from ml_bets')
         rows = self.cursor.fetchall()
         for row in rows:
-            if row[0] not in bet_ids:
-                bet_ids.append(row[0])
+            if row[0] not in spreadbet_ids:
+                mlbet_ids.append(row[0])
         ht = ''
         at = ''
         ht_score = -1
         at_score = -1
         for game in json:
-            if game['completed'] and game['id'] in bet_ids:
+            if game['completed'] and (game['id'] in spreadbet_ids or game['id'] in mlbet_ids) and game['id'] not in score_ids:
                 scores = game['scores']
                 for score in scores:
                     cur_team = score['name']
@@ -194,9 +229,66 @@ class APIinterface():
                     elif cur_team == game['away_team']:
                         at = cur_team
                         at_score = float(cur_score)
+                if game['id'] in spreadbet_ids:
+                    self.cursor.execute('select * from spread_bets where id = \'{}\''.format(game['id']))
+                    rows = self.cursor.fetchall()
+                    self.processSpreadBetResults(rows, ht, ht_score, at, at_score)
+                if game['id'] in mlbet_ids:
+                    self.cursor.execute('select * from ml_bets where id = \'{}\''.format(game['id']))
+                    rows = self.cursor.fetchall()
+                    self.processMLBetResults(rows,ht,ht_score,at,at_score)
                 self.cursor.execute('INSERT INTO scores VALUES (\'{}\',\'{}\',{},\'{}\',{})'.format(game['id'],ht,ht_score,at,at_score))
                  
-        
+
+    def processMLBetResults(self,ls,ht,hts,at,ats):
+        for row in ls:
+            team_bet_on = row[1]
+            book = row[2]
+            amt = float(row[3])
+            odds = float(row[4])
+            if team_bet_on == ht:
+                if hts > ats:
+                    #won bet
+                    payout = amt * odds
+                    self.updateBalance(book,payout)
+            elif team_bet_on == at:
+                if ats > hts:
+                    payout = amt * odds
+                    self.updateBalance(book,payout)
+
+    def processSpreadBetResults(self,ls, ht, hts, at, ats):
+        for row in ls:
+            team_bet_on = row[1]
+            book = row[2]
+            amt = float(row[3])
+            spread = float(row[4])
+            price = float(row[5])
+
+            if team_bet_on == ht:
+                if spread > 0:
+                    if ats-hts < spread:
+                        #won bet!
+                        payout = amt * price 
+                        self.updateBalance(book,payout)
+                elif spread < 0:
+                    spread = -1*spread
+                    if hts - ats > spread:
+                        #won bet
+                        payout = amt * price 
+                        self.updateBalance(book,payout)
+            elif team_bet_on == at:
+                if spread > 0:
+                    if hts-ats < spread:
+                        #won bet!
+                        payout = amt * price 
+                        self.updateBalance(book,payout)
+                elif spread < 0:
+                    spread = -1*spread
+                    if ats - hts > spread:
+                        #won bet
+                        payout = amt * price 
+                        self.updateBalance(book,payout)
+
 
 
 
